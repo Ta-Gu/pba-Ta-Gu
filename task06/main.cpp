117cbaf853aa4a64261eb7f1c84b3023831777b3
Akki-0 <61940255+Akki-0@users.noreply.github.com>
2023-06-07 23:28:18 +0900

    task06 finished

--- task06/main.cpp
+++ task06/main.cpp
@@ -47,11 +47,15 @@ void WdWddW_Spring3(
   // write code to correctly compute hessian of a spring.
   // ddw[i_node][j_node] stands for derivative of dw[i_node] w.r.t the end-point's position node2xyz[j_node]
   // the current hessian computed by the code below is not very accurate, so the simulation is unstable.
-  const Eigen::Matrix3d n = stiffness * u01 * u01.transpose();
-  ddw[0][0] = n;
-  ddw[1][1] = n;
-  ddw[0][1] = -n;
-  ddw[1][0] = -n;
+  // const Eigen::Matrix3d n = stiffness * u01 * u01.transpose();
+  // ddw[0][0] = n;
+  // ddw[1][1] = n;
+  // ddw[0][1] = -n;
+  // ddw[1][0] = -n;
+  ddw[0][0] = Eigen::Matrix3d::Identity() * stiffness - Eigen::Matrix3d::Identity() * stiffness * length_ini / length + (node2xyz[0] - node2xyz[1]) * (node2xyz[0] - node2xyz[1]).transpose() * stiffness * length_ini / (length * length * length);
+  ddw[0][1] = -1.f * (Eigen::Matrix3d::Identity() * stiffness - Eigen::Matrix3d::Identity() * stiffness * length_ini / length + (node2xyz[0] - node2xyz[1]) * (node2xyz[0] - node2xyz[1]).transpose() * stiffness * length_ini / (length * length * length));
+  ddw[1][0] = -1.f * (Eigen::Matrix3d::Identity() * stiffness - Eigen::Matrix3d::Identity() * stiffness * length_ini / length + (node2xyz[0] - node2xyz[1]) * (node2xyz[0] - node2xyz[1]).transpose() * stiffness * length_ini / (length * length * length));
+  ddw[1][1] = Eigen::Matrix3d::Identity() * stiffness - Eigen::Matrix3d::Identity() * stiffness * length_ini / length + (node2xyz[0] - node2xyz[1]) * (node2xyz[0] - node2xyz[1]).transpose() * stiffness * length_ini / (length * length * length);
 }
 
 float step_time_mass_spring_system_with_variational_integration(
